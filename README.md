b177c0f58ec70cdea58f5dfc7387892673ebbaba
mikhail.zotov <mkh.zotov@yandex.ru>
2020-12-15 01:28:26 +0400

    Implement an initial Spring Boot Web application

--- README.md
+++
@@ -1,1 +0,0 @@
-# passport-office-rest-service
---
+++ src/main/java/com/zotov/edu/passportofficerestservice/PassportOfficeRestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.zotov.edu.passportofficerestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassportOfficeRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassportOfficeRestServiceApplication.class, args);
+	}
+
+}
