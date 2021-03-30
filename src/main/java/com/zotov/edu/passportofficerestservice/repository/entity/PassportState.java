b5b22a602f486e4a27455f413c75f6bc0734d701
mikhail.zotov <mkh.zotov@yandex.ru>
2021-03-30 11:12:54 +0300

    Add name for the PassportState enum.

--- src/main/java/com/zotov/edu/passportofficerestservice/repository/entity/PassportState.java
+++ src/main/java/com/zotov/edu/passportofficerestservice/repository/entity/PassportState.java
@@ -1,6 +1,13 @@
 package com.zotov.edu.passportofficerestservice.repository.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public enum PassportState {
-    ACTIVE,
-    LOST
+    ACTIVE("ACTIVE"),
+    LOST("LOST");
+
+    private final String name;
 }
