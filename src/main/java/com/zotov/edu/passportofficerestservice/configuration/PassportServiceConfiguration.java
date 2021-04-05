a62a7e5bd06451890768d2f9ccfda5ae3702d834
mikhail.zotov <mkh.zotov@yandex.ru>
2021-04-05 23:33:30 +0300

    Add Spring's configuration annotation processor

--- src/main/java/com/zotov/edu/passportofficerestservice/configuration/PassportServiceConfiguration.java
+++ src/main/java/com/zotov/edu/passportofficerestservice/configuration/PassportServiceConfiguration.java
@@ -1,5 +1,6 @@
 package com.zotov.edu.passportofficerestservice.configuration;
 
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
@@ -8,6 +9,7 @@ import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import java.util.Locale;
 
 @Configuration
+@EnableConfigurationProperties(PassportServiceProperties.class)
 public class PassportServiceConfiguration {
     @Bean
     public LocaleResolver localeResolver() {
